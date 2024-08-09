d227490bd7ef9c893ac184baf7afb953de8d03bf
krishnakarne <krishnakarne@users.noreply.github.com>
2024-08-08 22:17:27 -0400

    Update coder.ts

--- coder.ts
+++ coder.ts
@@ -1,81 +1,12 @@
-addEventListener('message', ({ data }) => {
-  const { searchTerm, data: records } = data;
-
-  const filteredData = records.filter((item: any) => {
-    return Object.values(item).some(val =>
-      val && val.toString().toLowerCase().includes(searchTerm)
-    );
-  });
-
-  postMessage(filteredData);
-});
-
-
-
-applyFilter(searchTerm: string) {
-  if (!searchTerm) {
-    this.filteredTableData = [...this.branchSource.filteredData];
-    this.branchSource = new MatTableDataSource<any>(this.filteredTableData);
-    return;
-  }
-
-  // Using a Web Worker for heavy filtering operation
-  const worker = new Worker(new URL('./filter.worker', import.meta.url));
-  
-  worker.onmessage = ({ data }) => {
-    this.filteredTableData = data;
-    this.branchSource = new MatTableDataSource<any>(this.filteredTableData);
-  };
-
-  worker.postMessage({ data: this.branchSource.filteredData, searchTerm });
+.mat-paginator .mat-select-panel .mat-option:hover {
+  background-color: #FF5733 !important; /* Example custom hover color */
+  color: white !important; /* Example text color */
 }
 
-
-
-
-
-import { debounceTime, Subject } from 'rxjs';
-
-// Inside your component class
-searchSubject = new Subject<string>();
-
-ngOnInit() {
-  this.searchSubject.pipe(debounceTime(300)).subscribe((searchTerm) => {
-    this.applyFilter(searchTerm);
-  });
+.mat-paginator .mat-select-panel .mat-option.mat-active {
+  background-color: #FF5733 !important; /* Active state background color */
+  color: white !important; /* Active state text color */
 }
-
-onSearchText(event: Event) {
-  const searchTerm = (event.target as HTMLInputElement).value.trim().toLowerCase();
-  this.searchSubject.next(searchTerm);
-}
-
-applyFilter(searchTerm: string) {
-  if (!searchTerm) {
-    this.filteredTableData = [...this.branchSource.filteredData]; // Reset to full data
-    this.branchSource = new MatTableDataSource<any>(this.filteredTableData);
-    return;
-  }
-
-  this.filteredTableData = this.branchSource.filteredData.filter((item: any) => {
-    return this.searchInObject(item, searchTerm);
-  });
-
-  this.branchSource = new MatTableDataSource<any>(this.filteredTableData);
-}
----------------------------
-  searchInObject(obj: any, searchTerm: string): boolean {
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key) && obj[key] != null && obj[key] != undefined) {
-      const value = obj[key].toString().toLowerCase();
-      if (value.includes(searchTerm)) {
-        return true; // Early exit if a match is found
-      }
-    }
-  }
-  return false;
-}
-
 
 
 
